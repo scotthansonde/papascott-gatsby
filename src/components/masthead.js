39a9e9941dc07a5f158d89c7cff03c4f15099d75
papascott <papascott@gmail.com>
2018-07-13 20:13:29 +0200

    Add test message to title

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -4,7 +4,7 @@ export default ({metadata}) => (
   <div className="masthead">
   <div className="container">
     <h3 className="masthead-title">
-      <a href="/" title="Home">{metadata.title}</a>{' '}
+      <a href="/" title="Home">{metadata.title} Gatsby Test!</a>{' '}
       <small>{metadata.desc}</small>
     </h3>
   </div>
