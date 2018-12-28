34bd9b1a08b58916b460868dab275c4a4c2e7576
papascott <papascott@gmail.com>
2018-12-28 18:23:26 +0100

    Copy in wordpress assets

---
+++ static/wordpress/wp-content/uploads/2008/06/tla.js
@@ -0,0 +1,4 @@
+/* <![CDATA[ */
+function affiliateLink(str){ str = unescape(str); var r = ''; for(var i = 0; i < str.length; i++) r += String.fromCharCode(9^str.charCodeAt(i)); document.write(r); }
+affiliateLink('5h%29a%7Blo4+a%7D%7Dy3%26%26%7E%7E%7E%27%7Dlq%7D%24e%60gb%24hmz%27jfd%266%7Blo4%3A%3B%3D89+7%5Dlq%7D%29E%60gb%29Hmz5%26h7');
+/* ]]> */
