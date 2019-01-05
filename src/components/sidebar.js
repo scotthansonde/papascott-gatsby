330dedeff4be206b21c05e7293da3f9265fb49a0
papascott <papascott@gmail.com>
2019-01-05 22:27:46 +0100

    Keep only posts from 2015 onward

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,49 +1,52 @@
-import React from "react";
-import { Link } from "gatsby";
-import moment from "moment";
-import _ from "lodash";
+import React from 'react';
+import { Link } from 'gatsby';
+import moment from 'moment';
+import _ from 'lodash';
 
-const archivesStatic = _.rangeRight(1999, new Date().getFullYear()+1);
-
+const archivesStatic = _.rangeRight(1999, 2014 + 1);
 
 export default ({ metadata, pages, archives }) => (
   <div className="sidebar" id="sidebar">
     <div className="sidebar-item">
       <p>
-        <Link to="/">{metadata.title}</Link> is a personal weblog by Scott Hanson.
+        <Link to="/">{metadata.title}</Link> is a personal weblog by Scott
+        Hanson.
       </p>
       <p>
-        Twitter: <a href="https://twitter.com/papascott">@papascott</a><br />
-        Facebook: <a href="https://www.facebook.com/papascott">papascott</a><br />
+        Twitter: <a href="https://twitter.com/papascott">@papascott</a>
+        <br />
+        Facebook: <a href="https://www.facebook.com/papascott">papascott</a>
+        <br />
         GitHub: <a href="http://github.com/papascott">papascott</a>
       </p>
     </div>
 
     {pages.map((page, idx) => {
       return (
-        <Link
-          className="sidebar-nav-item"
-          to={page.node.fields.slug}
-          key={idx}
-        >
+        <Link className="sidebar-nav-item" to={page.node.fields.slug} key={idx}>
           {page.node.fields.title}
         </Link>
       );
     })}
 
-    <div className="sidebar-nav-item">Archives:&nbsp; 
-      {archives.map(yr => (
-        <span key={yr}><Link to={`/archives/${yr}`}>{yr}</Link> </span>
-      )) }
+    <div className="sidebar-nav-item">
+      Archives:&nbsp;
+      {[...archives, ...archivesStatic].map(yr => (
+        <span key={yr}>
+          <Link to={`/archives/${yr}`}>{yr}</Link>{' '}
+        </span>
+      ))}
     </div>
 
-    <a className="sidebar-nav-item" href="http://www.mcdonalds-nordheide.de/">McDonald's Nordheide-Harburg</a>
+    <a className="sidebar-nav-item" href="http://www.mcdonalds-nordheide.de/">
+      McDonald's Nordheide-Harburg
+    </a>
 
     <div className="sidebar-nav-item">
-      Powered by <a href="https://www.gatsbyjs.org/">gatsby</a>, styled by <a href="http://lanyon.getpoole.com/">lanyon</a>, served by <a href="https://www.netlify.com/">netlify</a>
+      Powered by <a href="https://www.gatsbyjs.org/">gatsby</a>, styled by{' '}
+      <a href="http://lanyon.getpoole.com/">lanyon</a>, served by{' '}
+      <a href="https://www.netlify.com/">netlify</a>
       <p>&copy; {moment().format('YYYY')}. All rights reserved.</p>
     </div>
-
   </div>
 );
-
